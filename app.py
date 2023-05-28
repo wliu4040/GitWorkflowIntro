fc3c5f3b0e857f71cf50e02fe147422d2f779e24
Utsab Saha <utsab.k.saha@gmail.com>
2023-05-28 02:59:11 +0000

    Added html templates for the summer 2023 cohort

--- app.py
+++ app.py
@@ -5,5 +5,13 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def students():
-    return render_template('students.html')
+def home():
+    return render_template('index.html')
+
+@app.route("/2022")
+def cohort_2022():
+    return render_template('students_2022.html')
+
+@app.route("/2023")
+def cohort_2023():
+    return render_template('students_2023.html')
