a78d4ab15a8e2d7de236e264879c2c5f6b876f85
AaronHamilton965 <91709196+AaronHamilton965@users.noreply.github.com>
2023-06-23 23:58:56 +0000

    Added team boba page

--- app.py
+++ app.py
@@ -20,3 +20,7 @@ def cohort_2023():
 @app.route("/nat_uts")
 def team_nat_uts():
     return render_template('teams/nat_uts.html')
+
+@app.route("/boba")
+def team_boba():
+    return render_template('teams/boba.html')
