513392adbf15fd75adb94e30c5c56f36bc59150f
ItzRikk <119906679+ItzRikk@users.noreply.github.com>
2023-06-23 01:53:32 +0000

    Add team page for Alvin and Ricardo

--- app.py
+++ app.py
@@ -20,3 +20,7 @@ def cohort_2023():
 @app.route("/nat_uts")
 def team_nat_uts():
     return render_template('teams/nat_uts.html')
+
+@app.route("/sleeper")
+def team_sleeper():
+    return render_template('teams/sleeper.html')
