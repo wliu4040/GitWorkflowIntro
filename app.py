23eb32680aed7a91e56159b8501540fc34c7249b
Yuancheng Cao <93391000+Cao1224@users.noreply.github.com>
2023-06-23 02:04:56 +0000

    first team commit

--- app.py
+++ app.py
@@ -20,3 +20,7 @@ def cohort_2023():
 @app.route("/nat_uts")
 def team_nat_uts():
     return render_template('teams/nat_uts.html')
+
+@app.route("/teamWork")
+def team_june22():
+    return render_template('teams/teamWork.html')
