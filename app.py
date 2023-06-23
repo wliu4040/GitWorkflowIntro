4708fa2b89fa21312e38ee0f9705836a86359f3b
iceypotato <40870742+iceypotato@users.noreply.github.com>
2023-06-23 01:44:17 +0000

    Added our blank team file as well as a route.

--- app.py
+++ app.py
@@ -20,3 +20,8 @@ def cohort_2023():
 @app.route("/nat_uts")
 def team_nat_uts():
     return render_template('teams/nat_uts.html')
+
+
+@app.route("/team_kn")
+def team_kn():
+    return render_template('teams/team_kn.html')
