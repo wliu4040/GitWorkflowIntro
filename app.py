ead4a0543bb8398cc546fd4eebcb9e283ac75165
UsmanT12 <144747599+UsmanT12@users.noreply.github.com>
2024-06-20 19:41:48 +0000

    Added team page for George Youkyoung and Usman

--- app.py
+++ app.py
@@ -109,3 +109,8 @@ def team_nick_val():
 @app.route("/luke_noah_maya")
 def team_luke_noah_maya():
     return render_template('teams/luke_noah_maya.html')
+
+@app.route("/team_5")
+def team_5():
+    return render_template('teams/team_5.html')
+
