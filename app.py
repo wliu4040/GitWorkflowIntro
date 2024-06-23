d2a10552b815d2fd9ec39a164d9c1a2f66d257d4
ChynnaSyas <152239931+ChynnaSyas@users.noreply.github.com>
2024-06-23 21:30:17 +0000

    Added team_CKL file

--- app.py
+++ app.py
@@ -121,3 +121,7 @@ def team_jin_allison_saad():
 @app.route("/brian_jia_honghao")
 def team_brian_jia_honghao():
     return render_template('teams/brian_jia_honghao.html')
+
+@app.route("/team_CKL")
+def team_chynna_kyongsu_lizzy():
+    return render_template('teams/team_CKL.html')
