7615bd2f2ffc89b756757ec9d7725aac9343cd3c
alszzy <99118533+alszzy@users.noreply.github.com>
2024-06-23 18:08:50 +0000

    added team page for pramodya and sherman

--- app.py
+++ app.py
@@ -121,3 +121,7 @@ def team_jin_allison_saad():
 @app.route("/brian_jia_honghao")
 def team_brian_jia_honghao():
     return render_template('teams/brian_jia_honghao.html')
+
+@app.route("/pramodya_sherman")
+def team_brian_jia_honghao():
+    return render_template('teams/pramo_sher.html')
