d2db69f73cfe8be6fb5a4557bf4d039a12ab2b11
Isaac <130631029+isa-hello@users.noreply.github.com>
2024-06-23 04:44:18 +0000

    created team entry

--- app.py
+++ app.py
@@ -121,3 +121,7 @@ def team_jin_allison_saad():
 @app.route("/brian_jia_honghao")
 def team_brian_jia_honghao():
     return render_template('teams/brian_jia_honghao.html')
+
+@app.route("/isaac_brody_benjamin")
+def team_isa_bro_ben():
+    return render_template('teams/isa_bro_ben.html')
