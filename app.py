073091935dacdc3c3a984126f4628e458486577c
Pranathi Chamarthi <150171527+psycompsci@users.noreply.github.com>
2024-06-24 06:14:32 +0000

    Added team page

--- app.py
+++ app.py
@@ -121,3 +121,7 @@ def team_jin_allison_saad():
 @app.route("/brian_jia_honghao")
 def team_brian_jia_honghao():
     return render_template('teams/brian_jia_honghao.html')
+
+@app.route("/julian_luis_pranathi")
+def team_jlp():
+    return render_template('teams/julian_luis_pranathi.html')
