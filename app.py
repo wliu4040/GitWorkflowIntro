f38d655fe46633c8228023bd17e075e72b0bd0e3
shanetto <162838940+shanetto@users.noreply.github.com>
2024-10-09 23:54:54 +0000

    Added team page for Rashida and Shane

--- app.py
+++ app.py
@@ -21,6 +21,9 @@ def cohort_2023():
 def cohort_2024():
     return render_template('students_2024.html')
 
+@app.route("/shane_rashida")
+def team_shane_rashida():
+    return render_template('teams/shane_rashida.html')
 
 @app.route("/nat_uts")
 def team_nat_uts():
