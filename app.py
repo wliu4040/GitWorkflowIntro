8bdd9a5abb576e5c52f39b96968db91255243198
Matthew Tran <114129299+MatthewTran22@users.noreply.github.com>
2024-10-10 19:10:54 +0000

    Added team page for Matthew, Dale, Suchith

--- app.py
+++ app.py
@@ -121,3 +121,7 @@ def team_jin_allison_saad():
 @app.route("/brian_jia_honghao")
 def team_brian_jia_honghao():
     return render_template('teams/brian_jia_honghao.html')
+
+@app.route("/matthew_dale_suchith")
+def matthew_dale_suchith():
+    return render_template('teams/matthew_dale_suchith.html')
