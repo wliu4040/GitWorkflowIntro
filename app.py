f77ca186fdae6b42ce60c5263de70bb9bdd16108
ajavadi1383 <124114446+ajavadi1383@users.noreply.github.com>
2024-10-10 03:04:39 +0000

    Made an empty team page.

--- app.py
+++ app.py
@@ -121,3 +121,7 @@ def team_jin_allison_saad():
 @app.route("/brian_jia_honghao")
 def team_brian_jia_honghao():
     return render_template('teams/brian_jia_honghao.html')
+
+@app.route("/amir_luis_austin")
+def team_amir_luis_austin():
+    return render_template('teams/amir_luis_austin.html')
