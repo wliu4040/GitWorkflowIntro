3a2c3cf4115c5e0818bf02ed5ef217a8435bdd33
Sydney Vo <sydneyvo098@gmail.com>
2024-10-10 04:38:11 +0000

    added team page for sydney ryan and pei-wan

--- app.py
+++ app.py
@@ -121,3 +121,7 @@ def team_jin_allison_saad():
 @app.route("/brian_jia_honghao")
 def team_brian_jia_honghao():
     return render_template('teams/brian_jia_honghao.html')
+
+@app.route("/team_sydney_ryan_peiwan")
+def team_sydney_ryan_peiwan():
+    return render_template('teams/team_sydney_ryan_peiwan.html')
