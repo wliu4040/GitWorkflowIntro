53f4a7add7c5515ff362c5f60198bdbb6e5de6e3
emyuans <147277376+emyuans@users.noreply.github.com>
2024-10-11 19:55:40 +0000

    Added team page for Emily, Isaac, and Jason

--- app.py
+++ app.py
@@ -121,3 +121,7 @@ def team_jin_allison_saad():
 @app.route("/brian_jia_honghao")
 def team_brian_jia_honghao():
     return render_template('teams/brian_jia_honghao.html')
+
+@app.route("/JIE")
+def team_JIE():
+    return render_template('teams/JIE.html')
