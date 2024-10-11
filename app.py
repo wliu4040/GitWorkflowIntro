f89867e991144fb9dff90633709f62cec082d950
Shawn Lin <115193840+shawnlin2@users.noreply.github.com>
2024-10-11 02:09:20 +0000

    added team file for team alex shawn chelsy

--- app.py
+++ app.py
@@ -121,3 +121,7 @@ def team_jin_allison_saad():
 @app.route("/brian_jia_honghao")
 def team_brian_jia_honghao():
     return render_template('teams/brian_jia_honghao.html')
+
+@app.route("/alex_shawn_chelsy")
+def team_alex_shawn_chelsy():
+    return render_template('teams/alex_shawn_chelsy.html')
