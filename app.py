de4d28470beb4e14ebeb58309b4990d2bd2e7217
JacobK <79630448+Waterberry71@users.noreply.github.com>
2024-11-16 00:02:19 +0000

    Added teampage

--- app.py
+++ app.py
@@ -277,3 +277,6 @@ def team_abdel_austin_chika():
 def team_evan_jared_keith_xuan():
     return render_template('teams/evan_jared_keith_xuan.html')
 
+@app.route("/edu_igo_jac")
+def team_edu_igo_jac():
+    return render_template('teams/edu_igo_jac.html')
