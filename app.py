00d044e61b31660602a672f45dca7ec3f12b9a6d
Kaulan Serzhanuly <143573989+KaulanSerzhanuly@users.noreply.github.com>
2025-02-01 00:51:14 +0000

    added team page for Kal_hec_ama

--- app.py
+++ app.py
@@ -316,3 +316,7 @@ def team_johnny_brent_drake():
 @app.route("/ahmet-khang-aizen")
 def team_ahmet_khang_aizen():
     return render_template('teams/ahmet-khang-aizen.html')
+
+@app.route("/kal_hec_ama")
+def kal_hec_ama():
+    return render_template('teams/kal_hec_ama.html')
